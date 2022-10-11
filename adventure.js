e6827b80ba940fabbfac6186669c1fb27bae4e28
Kwame Bullen <34757037+Adam43@users.noreply.github.com>
2022-10-11 13:49:04 -0400

    second commit

---
+++ adventure.js
@@ -0,0 +1,68 @@
+//Create the story.
+//BEGIN//
+//"You have discovered a cave. Do you want to walk in?"
+
+
+alert("Hello");
+//output Hello
+let name = prompt("I SAID HELLO, CHOSEN ONE! SPEAK YOUR NAME!");
+//input is saved under the name variable
+alert("Welcome " + name + "Welcome to your Journey which you will choose...");
+//string and var is outputted 
+alert("I will be the one to narrate your life. Now begin your perils!")
+let enterCave = confirm(name + ", it has seem that you have stubbled upon a cave. Do you want to walk in?");
+//alert(enterCave)
+// give user the option to confirm thier choice 
+//Story 1.1
+if (enterCave.lowerCase() == true) {
+   alert("You have choosen to go in! You are scared but brave and went in.");
+
+   let lightChoice = prompt("Now that you're in the cave you see that there are 3 flashlights to chose from. A 'medieval lighter', 'flash light', and 'iphone'. Which one do you choose?");
+   
+   if (lightChoice.lowerCase() == "lamp") {
+       alert("Cool, this is like the movies!");
+       alert("*Turn Lights On*");
+
+       let lampLight = confirm("Do you want to ")
+       //let lampLight = prompt("OH MY GOSHHH THERE'S A NASTY GROUP OF DARK WIZARDS");
+
+       if
+    }
+   }
+
+   
+ //  return yes //doesn't work 
+//user chooses "OK"
+} else if {
+    alert("You have decided to turn away and head to the woods because you are too terrified.");
+
+    let weapon = prompt("The moon light has spot lightend you some weapons for your trip. There is 'umbrella', 'twinky' or a 'bag of poop'...choose ONE!)
+
+}
+
+
+
+
+
+
+     
+
+
+else if (lightChoice.lowerCase() == "flash light"){
+    alert("This will do!")    
+} else {(lightChoice.lowerCase() == "iphone")
+    alert("Damn this on 10%")
+}
+}
+
+alert("You have chosen to use the " + lightChoice)
+alert("Continue ahead Young " + name)
+
+let spotChest = confirm("Is that a treasure chest you see?")
+alert(spotChest)
+if(spotChest == true){
+    let = treasureChest = prompt("Go on open it.")
+}else { 
+    let = treasureChest = prompt("You aren\'t adventurous at all smh")
+}
+//let treasureChest = prompt("Go on open it.")
